eb44372a2150d00378001a052341061f0ce18ba3
sdgosai <sachingosai.flexion@gmail.com>
2023-04-05 10:29:39 +0530

    first commit

---
+++ js-sheet-task.js
@@ -0,0 +1,14 @@
+// num to string
+var num = 20;
+var str = num.toString();
+// console.log(num); // 24
+// console.log(str); // "24"
+
+// Count character and word of given string
+var char = "sachin";
+// console.log(char.length); // 6
+
+// Seach specific word and its position from string
+var str = "hello john deo"
+var search = str.search("deo")
+console.log(search);
