f618efebcb6927c05d96b50d3167e9d534b67b2a
sdgosai <sachingosai.flexion@gmail.com>
2023-04-05 10:47:56 +0530

    - latest push add string methods.

--- js-sheet-task.js
+++ js-sheet-task.js
@@ -11,4 +11,17 @@ var char = "sachin";
 // Seach specific word and its position from string
 var str = "hello john deo"
 var search = str.search("deo")
-console.log(search);
+let position = str.search(/john/i);
+let result = str.includes("hello", 0);
+// console.log(search); // 11
+// console.log(position); // 6
+// console.log(result); // true
+
+// Swap given word with specified word from string replace word
+var text = "Hello my name is john deo & john deo is programmer.";
+let replace = text.replace("john deo", "sam deo");
+let rep = text.replace(/john deo/g, "sam deo");
+// console.log(replace); // sam deo
+// console.log(rep); // all is sam deo
+
+// Define numeric array and search given value from it
